dce4dd622e58672197eafa8e9af7597f7211cc11
Robin Hood <yoga3911@gmail.com>
2023-01-14 17:47:00 +0700

    add community screen

--- lib/screens/community/community.dart
+++ lib/screens/community/community.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp/dummy/community.dart';
+import 'package:whatsapp/screens/community/widgets/community_tile.dart';
 
 class CommunityScreen extends StatelessWidget {
   const CommunityScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Community"),
+    return Scaffold(
+      body: ListView(
+        children: [
+          const CommunityTileWidget(),
+          ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: communityData.length,
+            itemBuilder: (_, idx) {
+              final community = communityData[idx];
+              return CommunityTileWidget(community: community);
+            },
+          ),
+        ],
+      ),
     );
   }
 }
