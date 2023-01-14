3e8569288f4bf71c69c7adfe8da2ed1230ad6b8d
Robin Hood <yoga3911@gmail.com>
2023-01-14 14:01:52 +0700

    split chat tile widget

--- lib/screens/chat/chat.dart
+++ lib/screens/chat/chat.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:whatsapp/dummy/chats.dart';
-import 'package:whatsapp/themes/app_color.dart';
+import 'package:whatsapp/screens/chat/widgets/chat_tile.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -13,65 +12,7 @@ class ChatScreen extends StatelessWidget {
         itemCount: chatData.length,
         itemBuilder: (_, idx) {
           final chat = chatData[idx];
-          return ListTile(
-            title: Text(
-              chat.username,
-              style: const TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            subtitle: Text(
-              chat.message,
-              style: const TextStyle(
-                color: AppColor.grey1,
-              ),
-            ),
-            leading: CircleAvatar(
-              backgroundColor: AppColor.green1,
-              radius: 20,
-              child: Padding(
-                padding: EdgeInsets.all(chat.isStatus! ? 2 : 0),
-                child: ClipOval(
-                  child: Image.asset(chat.avatar!),
-                ),
-              ),
-            ),
-            trailing: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  DateFormat("HH:mm").format(
-                    DateTime.now(),
-                  ),
-                  style: TextStyle(
-                    color: chat.isMessage! ? AppColor.green1 : AppColor.grey1,
-                  ),
-                ),
-                const SizedBox(height: 5),
-                if (chat.isMessage!)
-                  CircleAvatar(
-                    backgroundColor: AppColor.green1,
-                    radius: 10,
-                    child: Text(
-                      chatData.length.toString(),
-                      style: const TextStyle(
-                        fontSize: 12,
-                        color: Colors.black,
-                      ),
-                    ),
-                  )
-                else if (chat.isMute!)
-                  const Icon(
-                    Icons.volume_off_rounded,
-                    color: AppColor.grey1,
-                  )
-              ],
-            ),
-            onTap: () {},
-          );
+          return ChatTileWidget(chat: chat);
         },
       ),
     );
