d9633d07368a4b582ae9db007d89924874fb9a3f
Robin Hood <yoga3911@gmail.com>
2023-01-14 14:01:25 +0700

    add total message and remove isMessage

--- lib/models/chat.dart
+++ lib/models/chat.dart
@@ -1,16 +1,16 @@
 class ChatModel {
   final String username;
   final String message;
+  final int total;
   final bool? isStatus;
-  final bool? isMessage;
   final bool? isMute;
   final String? avatar = "assets/cana.jpg";
 
   const ChatModel({
     required this.username,
     required this.message,
+    required this.total,
     this.isStatus = false,
-    this.isMessage = false,
     this.isMute = false,
   });
 }
