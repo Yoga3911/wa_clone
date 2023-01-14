5f00a4d0f491a3817ea8a20953eeea3579669a1d
Robin Hood <yoga3911@gmail.com>
2023-01-14 14:02:15 +0700

    add community screen

---
+++ lib/screens/community/community.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CommunityScreen extends StatelessWidget {
+  const CommunityScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text("Community"),
+    );
+  }
+}
