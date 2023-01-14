8af25517e292ecc47ce26de97be268813b80172d
Robin Hood <yoga3911@gmail.com>
2023-01-14 17:47:14 +0700

    add new grey color

--- lib/themes/app_color.dart
+++ lib/themes/app_color.dart
@@ -7,6 +7,7 @@ class AppColor {
   static const grey1 = Color(0xFFe5e5e5);
   static const grey2 = Color(0xFF232d36);
   static const grey3 = Color(0xFF101d25);
+  static const grey4 = Color(0xFF16242E);
 
   // ? GREEN
   static const green1 = Color(0xFF0ca996);
