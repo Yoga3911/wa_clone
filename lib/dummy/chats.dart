9b0f532b10a3aa687b749847c180d5e415c7a026
Robin Hood <yoga3911@gmail.com>
2023-01-14 13:22:53 +0700

    add dummy chat

---
+++ lib/dummy/chats.dart
@@ -0,0 +1,26 @@
+import 'package:whatsapp/models/chat.dart';
+
+const List<ChatModel> chatData = [
+  ChatModel(
+    username: "Eko",
+    message: "Mantap bang",
+    isMessage: true,
+    isStatus: true,
+  ),
+  ChatModel(
+    username: "Pras",
+    message: "Gaskan bang",
+    isMute: true,
+  ),
+  ChatModel(
+    username: "Ahmad",
+    message: "Siap laksanakan",
+    isMute: true,
+    isStatus: true,
+  ),
+  ChatModel(
+    username: "Budi",
+    message: "Bagi duit dong bang",
+    isMessage: true,
+  ),
+];
