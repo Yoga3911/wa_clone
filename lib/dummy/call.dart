7c4cd92f0aa9e38f86acdf5820da485ab6679419
Robin Hood <yoga3911@gmail.com>
2023-01-14 14:17:52 +0700

    add call model

---
+++ lib/dummy/call.dart
@@ -0,0 +1,19 @@
+import 'package:whatsapp/models/call.dart';
+
+List<CallModel> callData = [
+  CallModel(
+    username: "Eko",
+    callDate: DateTime(2023, 12, 12, 10),
+    isComingCall: false,
+  ),
+  CallModel(
+    username: "Ahmad",
+    callDate: DateTime(2023, 11, 12, 10),
+    isComingCall: true,
+  ),
+  CallModel(
+    username: "Sugeng",
+    callDate: DateTime(2023, 10, 12, 10),
+    isComingCall: true,
+  ),
+];
